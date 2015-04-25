24c3e1dde22e11398bf11c51da4f87882687b229
Belka <Evgenia_@email.ua>
2015-04-25 12:20:58 +0300

    project structure

---
+++ app/src/main/java/com/android_lessons/belkin/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,9 @@
+package com.android_lessons.belkin.nerdlauncher;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by Belkin on 24.04.2015.
+ */
+public class NerdLauncherFragment extends ListFragment {
+}
